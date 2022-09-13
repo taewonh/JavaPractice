ac6cf5282ce76f10e1710afe136031a97ea51a57
hnqtwn <40269387+taewonh@users.noreply.github.com>
2022-09-14 00:36:28 +0900

    fix: ReentrantLock 예제 코드 누락 수정
    - GitKraken 사용 미숙으로 누락된 파일 복구.

--- src/etc/thread/ReentrantLockTest.java
+++ src/etc/thread/ReentrantLockTest.java
@@ -1,2 +1,51 @@
-package etc.thread;public class ReentrantLockTest {
+package etc.thread;
+
+import java.io.PrintStream;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+    private final ReentrantLock lock = new ReentrantLock();
+    private final int totalCount = 15;
+    private int currentCount = 0;
+    private final int sleepTime = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLockTest lockTest = new ReentrantLockTest();
+        List<Callable<Void>> jobs = lockTest.generateJobs();
+        ExecutorService executorService = Executors.newFixedThreadPool(16);
+        LocalDateTime startTime = LocalDateTime.now();
+        executorService.invokeAll(jobs);
+
+        while (true) {
+            if (lockTest.currentCount == 15) {
+                break;
+            }
+        }
+        LocalDateTime endTime = LocalDateTime.now();
+        PrintStream var7 = System.out;
+        Duration between = Duration.between(startTime, endTime);
+        System.out.println(between.getSeconds() + "초");
+    }
+
+    private List<Callable<Void>> generateJobs() {
+        List<Callable<Void>> jobs = new ArrayList();
+        for(int count = 0; count < 15; ++count) {
+            jobs.add(() -> {
+                lock.lock();
+                Thread.sleep(1000L);
+                currentCount++;
+                lock.unlock();
+                return null;
+            });
+        }
+        return jobs;
+    }
 }
