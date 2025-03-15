1428912e37e257e3d8e9c3f992eb86696412ae6a
taewon.hong <40269387+taewonh@users.noreply.github.com>
2025-03-15 17:11:06 +0900

    test: JUnitPracticeTest 추가

---
+++ src/test/java/com/taewon/practice/dayonetest/JUnitPracticeTest.java
@@ -0,0 +1,81 @@
+package com.taewon.practice.dayonetest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class JUnitPracticeTest {
+
+    @Test
+    void assertEqualsTest() {
+        String expect = "Something";
+        String actual = "Something";
+
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test
+    void assertNotEqualsTest() {
+        String expect = "Something";
+        String actual = "Hello";
+
+        Assertions.assertNotEquals(expect, actual);
+    }
+
+    @Test
+    void assertTrueTest() {
+        Integer a = 1;
+        Integer b = 1;
+
+        Assertions.assertTrue(a.equals(b));
+    }
+
+    @Test
+    void assertFalseTest() {
+        Integer a = 10;
+        Integer b = 20;
+
+        Assertions.assertFalse(a.equals(b));
+    }
+
+    @Test
+    void assertThrowsTest() {
+        Assertions.assertThrows(RuntimeException.class,  () -> {
+            throw new RuntimeException("임의로 발생시킨 오류");
+        });
+    }
+
+    @Test
+    void assertNotNullTest() {
+        String value = "Hello";
+        Assertions.assertNotNull(value);
+    }
+
+    @Test
+    void assertNullTest() {
+        String value = null;
+        Assertions.assertNull(value);
+    }
+
+    @Test
+    void assertIterableTest() {
+        List<Integer> list1 = List.of(1, 2);
+        List<Integer> list2 = List.of(1, 2);
+        Assertions.assertIterableEquals(list1, list2);
+    }
+
+    @Test
+    void assertAllTest() {
+        String expect = "Something";
+        String actual = "Something";
+
+        List<Integer> list1 = List.of(1, 2);
+        List<Integer> list2 = List.of(1, 2);
+
+        Assertions.assertAll("Assert All", List.of(
+                () -> { Assertions.assertEquals(expect, actual); },
+                () -> { Assertions.assertIterableEquals(list1, list2);}
+        ));
+    }
+}
